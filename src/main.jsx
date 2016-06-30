97fb1db899421ced0554e7133ca2e0c735305b06
Alexander Kozovski <kozovski.a@gmail.com>
2016-06-30 11:06:02 -0400

    adding gitignore

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,4 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var ListManager = require('./components/ListManager.jsx')
 
 ReactDOM.render(<ListManager title ="Ingredients" />, document.getElementById('ingredients'));
-ReactDOM.render(<ListManager title ="ToDo" />, document.getElementById('todo'));
-ReactDOM.render(<ListManager headingColor="yellow" title ="Music" />, document.getElementById('music'));
-ReactDOM.render(<ListManager headingColor="tomato" title ="Brian" />, document.getElementById('brian'));
